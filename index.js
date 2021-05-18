71f4b324b0554fa73dfbc13729d7a1b0bc5b5075
codingno <codingno@gmail.com>
2021-05-18 16:26:17 +0700

    initial webrtc

---
+++ index.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const fs = require('fs');
+const key = fs.readFileSync('./key.pem');
+const cert = fs.readFileSync('./cert.pem');
+const express = require('express')
+const app = express()
+// const http = require('http')
+const https = require('https');
+// const server = http.createServer(app)
+const server = https.createServer({key: key, cert: cert }, app);
+const { Server } = require("socket.io")
+const io = new Server(server)
+const { ExpressPeerServer } = require('peer');
+const { v4: uuidv4 } = require('uuid')
+
+const PORT = process.env.PORT || 443
+const peerServer = ExpressPeerServer(server);
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use('/peerserver', peerServer)
+
+app.get('/', (req, res) => {
+    const user_id = uuidv4()
+    res.redirect(`/${user_id}`)
+})
+
+
+app.get('/:room', (req, res) => {
+    res.render('index', { user_id : req.params.room, peer_port : PORT + 1})
+})
+
+io.on('connection', (socket) => {
+socket.on('webrtc', (room_id, user) => {
+        console.log('user ' + user + ' join in room ' + room_id);
+        socket.join(room_id)
+        socket.broadcast.to(room_id).emit('webrtc', room_id, user)
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+            socket.broadcast.to(room_id).emit('user-disconnected', user)
+        });
+    });
+})
+
+server.listen(PORT, () => console.log('listening on PORT:' + PORT))
---
+++ public/script.js
@@ -0,0 +1,63 @@
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+const peer = new Peer(undefined, {
+    host: '/',
+    path: '/peerserver',
+    port: 443,
+    secure: true
+});
+
+const peers = {}
+
+const myVideo = document.createElement('video')
+myVideo.muted = true
+myVideo.id = 'video1'
+
+navigator.mediaDevices.getUserMedia({
+    video : true,
+    audio : true
+}).then( stream => {
+    addVideoStream(myVideo, stream)
+
+    peer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+    })
+
+    socket.on('webrtc', (room_id, id) => {
+        console.log('User connected : ' + id);
+        connectToNewUser(id, stream)
+    })
+})
+
+socket.on('user-disconnected', id => {
+    if(peers[id]) peers[id].close()
+})
+
+peer.on('open', id => {
+    socket.emit('webrtc', user_id, id)
+})
+
+function connectToNewUser(id, stream) {
+    const call = peer.call(id, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () => {
+        video.remove()
+    })
+    peers[id] = call
+}
+
+function addVideoStream(video, stream) {
+   video.srcObject = stream
+   video.addEventListener('loadedmetadata', () => {
+       video.play()
+   }) 
+   videoGrid.append(video)
+}
+
